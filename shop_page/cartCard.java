1a37a88805c1cbf42e70b29710061385178ed9fb
thezenigma <khannafizfuad@gmail.com>
2025-09-19 09:29:17 +0600

    New Changes

--- shop_page/cartCard.java
+++ shop_page/cartCard.java
@@ -1,5 +1,12 @@
 package shop_page;
 
-public class cartCard {
+import java.awt.Color;
 
+import javax.swing.*;
+public class cartCard extends JPanel{
+    public cartCard() {
+        this.setLayout(null);
+        this.setBounds(10, 90, 340, 150);
+        this.setBackground(Color.WHITE);
+    }
 }
